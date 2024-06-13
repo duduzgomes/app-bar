bd8c764ab6d81f34aafe43314631de7a05ef183b
Eduardo Gomes <carlosgomesduduz@gmail.com>
2024-06-13 10:09:17 -0300

    Criação dos dtos

---
+++ src/main/java/com/eduardogomes/gomes_bar/dto/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.eduardogomes.gomes_bar.dto;
+
+public record LoginRequest(String username, String password) {
+    
+}
---
+++ src/main/java/com/eduardogomes/gomes_bar/dto/LoginResponse.java
@@ -0,0 +1,5 @@
+package com.eduardogomes.gomes_bar.dto;
+
+public record LoginResponse(String accessToken, Long expiresIn) {
+    
+}
